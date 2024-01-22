57e2e6a5814828cb2c9ecabfc361e6204bc72a86
yunuSdet <yunuskulcu84@gmail.com>
2024-01-23 02:57:26 +0300

    Optisons for 'Seviye seçin' are updated

--- levels.php
+++ levels.php
@@ -101,8 +101,7 @@ include 'navbar.php';
                   <option selected value=1>1</option>
                   <option value=2>2</option>
                   <option value=3>3</option>
-                  <option value=4>4</option>
-                  <option value=5>5</option>
+   
                  
                   
                 </select>
