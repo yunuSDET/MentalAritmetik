a985143e113ebba27387309d9947df1177341bf1
yunuSdet <yunuskulcu84@gmail.com>
2024-01-31 14:33:26 +0300

    footer.php is updated

--- footer.php
+++ footer.php
@@ -13,28 +13,42 @@ if (session_status() == PHP_SESSION_NONE) {
 .footer {
     position: fixed;
     bottom: 0;
+ 
 
     width: 100%;
     background-color: #f8f9fa; /* Footer arkaplan rengi */
-    padding: 10px; /* Footer içeriği ile kenar boşluğu */
+    padding-top: 10px; /* Footer içeriği ile kenar boşluğu */
     text-align: center; /* Footer içeriğini ortala */
 }
+.before-footer {
+   
+    
+ 
+
+ 
+    padding-top: 200px; /* Footer içeriği ile kenar boşluğu */
+ 
+}
+
 
 </style>
+
+<div class="before-footer">
+
+
+</div>
+
 
 <div class="footer">
 
 
-<div class="card text-center">
-<div class="card-header">
-  Developed by <a href="https://www.linkedin.com/in/yunus-kulcu/">Kulcu</a>
-</div>
+ 
 <div class="card-body">
    
-    <footer class="footer">İstek ve önerileriniz için <a href="
+    <footer class="footer">Developed by <a href="https://www.linkedin.com/in/yunus-kulcu/">Kulcu</a> - İstek ve önerileriniz için <a href="
     contact-us.php">tıklayın</a></footer>
  
-</div>
+ 
 </div>
 
 </div>
