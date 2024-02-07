fd227b0694dbd7408035e5bf1ebdc3c5a661908c
yunuSdet <yunuskulcu84@gmail.com>
2024-02-07 11:30:21 +0300

    Profile page is updated for all users

--- task.php
+++ task.php
@@ -1,7 +1,6 @@
 <?php
 include 'sessionManager.php';
 checkUserSession();
-include 'navbar.php';
 
 // Admin kontrolü
 if (isset($_SESSION['user']) && $_SESSION['user'] === 'admin') {
@@ -10,7 +9,7 @@ if (isset($_SESSION['user']) && $_SESSION['user'] === 'admin') {
     $db = new SQLite3($databaseFile);
 
     // SELECT sorgusu
-    $query = "SELECT users.id AS userId, users.username AS userUsername, scores.score, scores.zaman_damgasi, scores.pageName
+    $query = "SELECT users.id AS userId, users.username, scores.score, scores.zaman_damgasi, scores.pageName
               FROM users
               LEFT JOIN scores ON users.id = scores.userId
               ORDER BY users.id, scores.zaman_damgasi DESC";
@@ -20,7 +19,7 @@ if (isset($_SESSION['user']) && $_SESSION['user'] === 'admin') {
     $userScores = array();
     while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
         $userScores[$row['userId']][] = array(
-            'username' => $row['userUsername'],
+            'username' => $row['username'],
             'score' => $row['score'],
             'zaman_damgasi' => $row['zaman_damgasi'],
             'pageName' => $row['pageName']
@@ -68,13 +67,11 @@ if (isset($_SESSION['user']) && $_SESSION['user'] === 'admin') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <!-- Bootstrap CSS -->
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Görev Tamamlama Listesi</title>
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="container bg-light">
+    <div class="container bg-primary-subtle">
         <div class="row">
             <div class="col-sm-12 mt-4">
                 <h2 style="text-align:center">Görev Tamamlama Listesi</h2>
@@ -84,7 +81,7 @@ if (isset($_SESSION['user']) && $_SESSION['user'] === 'admin') {
                     // Admin ise
                     echo "<form method=\"POST\" action=\"\" class=\"col-4 offset-4\">";
                     echo "<label for=\"userList\">Kullanıcı Seç:</label>";
-                    echo "<select id=\"userList\" name=\"userList\" class=\"form-control\">";
+                    echo "<select id=\"userList\" name=\"userList\">";
                     foreach ($userScores as $userId => $scores) {
                         $username = $scores[0]['username'];
                         echo "<option value=\"$userId\">$username</option>";
@@ -104,12 +101,11 @@ if (isset($_SESSION['user']) && $_SESSION['user'] === 'admin') {
                     echo "<div class='container mt-4'>";
                     echo "<h3>Görev Verilen Kullanıcının Skorları:</h3>";
                     echo "<table class='table'>";
-                    echo "<thead><tr><th>Username</th><th>Score</th><th>Zaman Damgası</th><th>Sayfa Adı</th><th>Skor Durumu</th></tr></thead>";
+                    echo "<thead><tr><th>Score</th><th>Zaman Damgası</th><th>Sayfa Adı</th><th>Skor Durumu</th></tr></thead>";
                     echo "<tbody>";
 
                     foreach ($selectedUserScores as $scoreRow) {
                         echo "<tr>";
-                        echo "<td>{$scoreRow['username']}</td>";
                         echo "<td>{$scoreRow['score']}</td>";
                         echo "<td>{$scoreRow['zaman_damgasi']}</td>";
                         echo "<td>";
@@ -169,20 +165,5 @@ if (isset($_SESSION['user']) && $_SESSION['user'] === 'admin') {
             </div>
         </div>
     </div>
- 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
-    
-
-    
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-    -->
 </body>
 </html>
